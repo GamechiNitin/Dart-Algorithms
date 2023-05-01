4ca667b925a07f5f70de944a9c878db082d7e013
GamechiNitin <nitingamechi1696@gmail.com>
2023-05-01 17:35:52 +0530

    Dart : String - Concatenation & Interpolation

---
+++ EffectiveDart/types/string/concatenation.dart
@@ -0,0 +1,15 @@
+void main() {
+  var m1 = "I";
+  var m2 = "Love";
+  var m3 = "India";
+  int a = 5;
+  int b = 8;
+
+  // Concatenation : using +
+  print(m1 + " " + m2 + " " + m3);
+
+  // Interpolation
+  print("\nString Interpolation $m1 $m2 $m3");
+  print("String Interpolation $m1 $m2 $m3".toUpperCase());
+  print("${a * b}");
+}
