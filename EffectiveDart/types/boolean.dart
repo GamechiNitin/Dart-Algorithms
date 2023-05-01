bac7b35f5f6bd2e3ced5a7468e433d4edd6d71cc
GamechiNitin <nitingamechi1696@gmail.com>
2023-05-01 17:05:05 +0530

    Dart : Boolean

---
+++ EffectiveDart/types/boolean.dart
@@ -0,0 +1,21 @@
+/* 
+   To run the program navigate to specific folder in terminal
+   : dart run filename.dart
+*/
+void main() {
+  /* Boolean : true | false 
+   Nullable Boolean : true | false | null
+
+   Compile-time constants
+  */
+
+  // Check for an empty String
+  String month = "";
+  print(month.isEmpty);
+
+  // Check given value is Even or Not
+  int isEvenValue = 5;
+  print(isEvenValue.isEven);
+
+  
+}
