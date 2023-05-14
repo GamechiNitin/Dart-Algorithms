da20ef33b8ecdaac271596e32c86b2433468b8b8
GamechiNitin <nitingamechi1696@gmail.com>
2023-05-14 22:50:10 +0530

    Dart : Sets Difference

---
+++ EffectiveDart/types/sets/difference.dart
@@ -0,0 +1,29 @@
+/* 
+   To run the program navigate to specific folder in terminal
+   : dart run filename.dart
+*/
+void main() {
+/* 
+  Sets : A set in Dart is an unordered collection of unique items.
+  Support Type inference.
+  Unordered Array with unique input.
+
+  Union : Creates a new set which contains all the elements of this set and [other].
+  Intersection : Creates a new set which is the intersection between this set and [other].
+  Difference : Creates a new set with the elements of this that are not in [other].
+*/
+  var group1 = <String>{};
+  var group2 = <String>{};
+
+  group1.addAll({"India", "Australia", "New Zealand", "Nepal", "Zimbawe"});
+  group2.addAll({"Sri Lanka", "West Indies", "Zimbawe", "Bangladesh", "Nepal"});
+
+  // Difference of 2 sets
+  print(group1.difference(group2));
+  print(group2.difference(group1));
+
+/* Difference Output :
+  {India, Australia, New Zealand}
+  {Sri Lanka, West Indies, Bangladesh}
+*/
+}
